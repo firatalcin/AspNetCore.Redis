eca68facc13a6413e8fb99bfcf4e183423cd5fcd
Fırat Alçın <firatalcin04@gmail.com>
2024-04-16 23:22:40 +0300

    Asp.Net Core ile In-Memory Caching ve (Redis)Distributed Caching Uygulama

---
+++ Source_GY/Redis_Cache_Example/InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("setName/{name}")]
+        public void SetName(string name)
+        {
+            _memoryCache.Set("name", name);
+        }
+
+        [HttpGet]
+        public string GetName()
+        {
+            if(_memoryCache.TryGetValue("name", out string name))
+            {
+                return _memoryCache.Get<string>("name");
+            }
+            return "";
+        }
+
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+
+        [HttpGet("getDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+    }
+}
