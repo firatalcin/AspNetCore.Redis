8a82219d0fbe801393e092b3121c29aa7c2e6008
Fırat Alçın <firatalcin04@gmail.com>
2024-04-17 23:06:08 +0300

    Pub/Sub Özelliği - Message Broker

---
+++ Source_GY/Redis_Cache_Example/Redis.Pub.Sub.Publisher.Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using StackExchange.Redis;
+
+namespace Redis.Pub.Sub.Publisher.Example
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await PublishMessagesAsync();
+        }
+
+        static async Task PublishMessagesAsync()
+        {
+            ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+            ISubscriber subscriber = connection.GetSubscriber();
+
+            while (true)
+            {
+                Console.Write("Mesaj: ");
+                string message = Console.ReadLine();
+                await subscriber.PublishAsync("mychannel", message);
+            }
+        }
+    }
+}
---
+++ Source_GY/Redis_Cache_Example/Redis.Pub.Sub.Subsciber.Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using StackExchange.Redis;
+
+namespace Redis.Pub.Sub.Subsciber.Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+            ISubscriber subscriber = connection.GetSubscriber();
+
+            subscriber.SubscribeAsync("mychannel", (channel, message) =>
+            {
+                Console.WriteLine(message);
+            });
+
+            Console.Read();
+        }
+    }
+}
